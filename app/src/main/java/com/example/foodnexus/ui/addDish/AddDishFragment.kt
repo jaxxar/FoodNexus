8e5dd664c4a160a632c0300faa4a8e7d6dc1f742
Dimitris <dimitrisgkinis1990@gmail.com>
2021-03-08 00:04:47 +0200

    version 0.1.1 Added AddDishFragment

---
+++ app/src/main/java/com/example/foodnexus/ui/addDish/AddDishFragment.kt
@@ -0,0 +1,27 @@
+package com.example.foodnexus.ui.addDish
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.foodnexus.databinding.FragmentAddDishBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AddDishFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentAddDishBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAddDishBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.dialog.text = "Test"
+    }
+}
--- app/src/main/java/com/example/foodnexus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/foodnexus/ui/home/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.example.foodnexus.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.foodnexus.R
 import com.example.foodnexus.databinding.FragmentHomeBinding
 
@@ -16,17 +14,37 @@ class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var binding: FragmentHomeBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater)
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             binding.textHome.text = it
         })
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_dish_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_add_dish -> {
+                val action = HomeFragmentDirections.actionNavigationHomeToAddDishFragment()
+                findNavController().navigate(action)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
