51c30ec220f31917e3b27dcc0b83ec6c7711f959
Dimitris <dimitrisgkinis1990@gmail.com>
2021-03-07 18:15:26 +0200

    version 0.1.0 Added ViewBinding

--- app/src/main/java/com/example/foodnexus/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/foodnexus/ui/dashboard/DashboardFragment.kt
@@ -4,28 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.foodnexus.R
+import com.example.foodnexus.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDashboardBinding.inflate(inflater)
         dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            binding.textDashboard.text = it
         })
-        return root
+        return binding.root
     }
 }
--- app/src/main/java/com/example/foodnexus/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/foodnexus/ui/notifications/NotificationsFragment.kt
@@ -4,28 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.foodnexus.R
+import com.example.foodnexus.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var binding: FragmentNotificationsBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNotificationsBinding.inflate(inflater)
         notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            binding.textNotifications.text = it
         })
-        return root
+        return binding.root
     }
 }
