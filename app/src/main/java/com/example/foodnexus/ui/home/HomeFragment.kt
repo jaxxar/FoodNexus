7f74f2b047edf3cf3de4c377071604c6e3c80687
Dimitris <dimitrisgkinis1990@gmail.com>
2021-03-07 17:25:32 +0200

    Initial commit with splash screen

---
+++ app/src/main/java/com/example/foodnexus/ui/home/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.foodnexus.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodnexus.R
+import com.example.foodnexus.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            binding.textHome.text = it
+        })
+        return binding.root
+    }
+}
