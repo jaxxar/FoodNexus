84c22cfe5a14d360d593eee45335823a0a7c235c
Dimitris <dimitrisgkinis1990@gmail.com>
2021-03-10 23:59:08 +0200

    version 0.2.0 Added image select functionality

--- app/src/main/java/com/example/foodnexus/ui/addDish/AddDishFragment.kt
+++ app/src/main/java/com/example/foodnexus/ui/addDish/AddDishFragment.kt
@@ -1,7 +1,16 @@
 package com.example.foodnexus.ui.addDish
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +19,14 @@ import com.example.foodnexus.R
 import com.example.foodnexus.databinding.DialogSelectImageBinding
 import com.example.foodnexus.databinding.FragmentAddDishBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class AddDishFragment : BottomSheetDialogFragment() {
 
@@ -84,12 +101,96 @@ class AddDishFragment : BottomSheetDialogFragment() {
         dialog?.setContentView(binding.root)
 
         binding.cameraImageButton.setOnClickListener {
+            Dexter.withContext(context).withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permisions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    displayGoToSettingsDialog()
+                }
+            }).onSameThread().check()
             dialog?.dismiss()
         }
         binding.galleryImageButton.setOnClickListener {
+            Dexter.withContext(context).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : PermissionListener {
+
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    val intent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(intent, STORAGE)
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    Permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    displayGoToSettingsDialog()
+                }
+
+            }).onSameThread().check()
             dialog?.dismiss()
         }
         dialog?.show()
     }
 
+    private fun displayGoToSettingsDialog() {
+        AlertDialog.Builder(context)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", context?.packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.extras?.let {
+                    val thumbnail: Bitmap = data.extras!!.get("data") as Bitmap
+                    binding.image.setImageBitmap(thumbnail)
+                }
+            }
+            if (requestCode == STORAGE) {
+                data?.let {
+                    val selectedImageURI = data.data
+                    binding.image.setImageURI(selectedImageURI)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA = 1
+        private const val STORAGE = 2
+    }
 }
